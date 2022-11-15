98ca8b0158c56c751700264da785fa1027a992f0
syoung1234 <syoung8818@gmail.com>
2022-11-15 22:45:28 +0900

    Add setting

--- src/main.js
+++ src/main.js
@@ -5,8 +5,11 @@ import router from "./router";
 import "./assets/css/nucleo-icons.css";
 import "./assets/css/nucleo-svg.css";
 import ArgonDashboard from "./argon-dashboard";
+import axios from 'axios'
+
 
 const appInstance = createApp(App);
+appInstance.config.globalProperties.$axios = axios; 
 appInstance.use(store);
 appInstance.use(router);
 appInstance.use(ArgonDashboard);
