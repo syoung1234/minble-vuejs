02edc810cd4392c8b5b4334abf7cae04b14bc452
syoung1234 <syoung8818@gmail.com>
2022-11-16 13:40:34 +0900

    Add axios, validation

--- src/views/Signup.vue
+++ src/views/Signup.vue
@@ -87,12 +87,37 @@
             </div>
             <div class="card-body">
               <form role="form">
-                <argon-input type="email" placeholder="이메일" aria-label="Email" v-model="email" />
-                <argon-input type="password" placeholder="비밀번호" aria-label="Password" v-model="password" />
-                <argon-input type="password_confirmation" placeholder="비밀번호 확인" aria-label="PasswordConfirmation" v-model="password_confirmation" />
-                <argon-input type="text" placeholder="닉네임" aria-label="Nickname" v-model="nickname" />
-                <argon-input type="text" placeholder="휴대폰 번호" aria-label="Nickname" v-model="phone" />
-                <argon-checkbox checked>
+                <input class="form-control form-control-default invalid" type="email" placeholder="이메일" aria-label="email" v-model="email" @blur="emailValidCheck(), checkDuplicate('email')" />
+                  <div class="text-muted font-italic" v-if="!emailValidCheckFlag"> 
+                    <small class="color-red">이메일 형식으로 입력해주세요.</small>
+                  </div>
+                  <div class="text-muted font-italic" v-else-if="emailDuplicateFlag === false"> 
+                    <small class="color-red">이미 사용 중인 이메일입니다.</small>
+                  </div>
+                  <div class="text-muted font-italic" v-else-if="emailDuplicateFlag === true"> 
+                    <small class="color-green">사용 가능한 이메일입니다.</small>
+                  </div>
+                <input class="form-control form-control-default invalid mt-3" type="password" placeholder="비밀번호" aria-label="Password" v-model="password" @blur="passwordValid(), passwordCheckValid()" />
+                  <div class="text-muted font-italic" v-if="!passwordValidFlag"> 
+                    <small class="color-red">8~16자 영문, 숫자를 사용하세요.</small>
+                  </div>
+                  <input class="form-control form-control-default invalid mt-3" type="password" placeholder="비밀번호 확인" aria-label="PasswordConfirmation" v-model="password_confirmation" @blur="passwordCheckValid" />
+                  <div class="text-muted font-italic" v-if="!passwordCheckFlag">
+                    <small class="color-red">비밀번호가 일치하지 않습니다.</small>
+                  </div>
+                <input class="form-control form-control-default invalid mt-3" type="text" placeholder="닉네임" aria-label="Nickname" v-model="nickname" @blur="nicknameNullCheck(), checkDuplicate('nickname')" />
+                  <div class="text-muted font-italic" v-if="!nicknameNullCheckFlag"> 
+                      <small class="color-red">닉네임 입력해주세요.</small>
+                  </div>
+                  <div class="text-muted font-italic" v-else-if="nicknameDuplicateFlag === false"> 
+                    <small class="color-red">이미 사용 중인 닉네임입니다.</small>
+                  </div>
+                  <div class="text-muted font-italic" v-else-if="nicknameDuplicateFlag === true"> 
+                    <small class="color-green">사용 가능한 닉네임입니다.</small>
+                  </div>
+                <input class="form-control form-control-default invalid mt-3" type="text" placeholder="휴대폰 번호" aria-label="Nickname" v-model="phone" />
+                <div class="form-check"> 
+                  <input type="checkbox" class="form-check-input" v-model="agreeCheck">
                   <label class="form-check-label" for="flexCheckDefault">
                     I agree the
                     <a
@@ -100,9 +125,10 @@
                       class="text-dark font-weight-bolder"
                     >Terms and Conditions</a>
                   </label>
-                </argon-checkbox>
+                </div>
+                
                 <div class="text-center">
-                  <argon-button fullWidth color="dark" variant="gradient" class="my-4 mb-2">Sign up</argon-button>
+                  <button class="btn mb-0 bg-gradient-dark btn-md w-100 null my-4 mb-2" type="button" fullWidth color="dark" variant="gradient" @click="postData">Sign up</button>
                 </div>
                 <p class="text-sm mt-3 mb-0">
                   Already have an account?
@@ -126,19 +152,129 @@
 <script>
 import Navbar from "@/examples/PageLayout/Navbar.vue";
 import AppFooter from "@/examples/PageLayout/Footer.vue";
-import ArgonInput from "@/components/ArgonInput.vue";
-import ArgonCheckbox from "@/components/ArgonCheckbox.vue";
-import ArgonButton from "@/components/ArgonButton.vue";
 const body = document.getElementsByTagName("body")[0];
-
+const axiosConfig = {
+        headers:{
+            "Content-Type": "application/json"
+        }
+    }
 export default {
   name: "signin",
+  data() {
+        return {
+            email: null,
+            nickname: null,
+            password: null,
+            password_confirmation: null,
+            phone: null,
+            passwordValidFlag: true,
+            passwordCheckFlag: true,
+            nicknameNullCheckFlag: true,
+            emailValidCheckFlag: true,
+            emailDuplicateFlag: '',
+            nicknameDuplicateFlag: '',
+            agreeCheck: false,
+        }
+    },
+    methods: {
+        // 중복체크 (이메일, 닉네임)
+        checkDuplicate(key) {
+          let data = {};
+          let type = null;
+          data.email = this.email;
+          data.nickname = this.nickname;
+          let result = null;
+
+          if (key == "email") {
+              type = this.email;
+          } else if (key == "nickname") {
+              type = this.nickname;
+          }
+          if (type != null) {
+              this.$axios
+              .post(`/api/duplicate/${key}`, data, axiosConfig)
+              .then((res) => {
+                  if (res.data == "exist") {
+                      result = false;
+                  } else {
+                      result = true;
+                  }
+
+                  if (key == "email") {
+                      this.emailDuplicateFlag = result;
+                  } else if (key == "nickname") {
+                      this.nicknameDuplicateFlag = result;
+                  }
+                      
+              })
+              .catch((error) => {
+                console.log(error);
+              })
+          }
+            
+        },
+        // 이메일 유효성
+        emailValidCheck() {
+	        if (/.+@.+\..+/.test(this.email)) {
+	        	//유효성이 틀리다면 data 값을 false로 한다.
+                this.emailValidCheckFlag = true;
+	        } else {
+	        	this.emailValidCheckFlag = false;
+	        }
+        },
+        // 닉네임 null 체크
+        nicknameNullCheck() {
+            if (this.nickname == null) {
+                this.nicknameNullCheckFlag = false;
+            } else {
+                this.nicknameNullCheckFlag =true
+            }
+        },
+        // 비밀번호 유효성
+        passwordValid () {
+          if (/^(?=.*[a-z])(?=.*[0-9]).{8,16}$/.test(this.password)) {
+            this.passwordValidFlag = true
+          } else {
+            this.passwordValidFlag = false
+          }
+        },
+        // 비밀번호 확인
+        passwordCheckValid() {
+            if(this.password === this.password_confirmation) {
+                this.passwordCheckFlag = true
+            } else {
+                this.passwordCheckFlag = false
+            }
+        },
+        // 회원가입 전송
+        postData() {
+            if (this.email != null && this.password != null && this.password_confirmation != null && this.nickname != null &&
+            this.passwordValidFlag == true && this.passwordCheckFlag == true && this.agreeCheck == true) {
+                let saveData = {};
+                saveData.email = this.email;
+                saveData.password = this.password;
+                saveData.nickname= this.nickname;
+                saveData.phone = this.phone;
+
+                this.$axios
+                .post("/api/register", JSON.stringify(saveData), axiosConfig)
+                .then((res) => {
+                  console.log(res)
+                  if (res.data == "success") {
+                      this.$router.push("/");
+                  }
+                })
+                .catch((error) => { 
+                  console.log(error);
+                })
+                .finally(() => {
+                });
+            }
+        },
+  },
   components: {
     Navbar,
     AppFooter,
-    ArgonInput,
-    ArgonCheckbox,
-    ArgonButton,
   },
   created() {
     this.$store.state.hideConfigButton = true;
