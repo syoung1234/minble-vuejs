02b3158309c6a61b76c66d78df0c21a4f1a67e21
syoung1234 <syoung8818@gmail.com>
2022-11-24 20:59:30 +0900

    Fix close button style

--- src/examples/PostModal.vue
+++ src/examples/PostModal.vue
@@ -5,9 +5,12 @@
                 <div class="modal-container">
                     <div class="align-right">
                         <slot name="header">  </slot>
-                        <button type="button" class="shadow-none navbar-toggler ms-2" @click="$emit('close')">
-                            <span>X</span>
+                        <!-- <button type="button" class="shadow-none navbar-toggler ms-2" @click="$emit('close')"> -->
+                        <button type="button" @click="$emit('close')" class="p-0 btn btn-link text-dark fixed-plugin-close-button">
+                            <i class="fa fa-close" aria-hidden="true"></i>
                         </button>
+                            <!-- <span>X</span>
+                        </button> -->
                     </div>
 
                     <div class="modal-body">
