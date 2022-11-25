e7a754ee8c5556d00d039855731a930d27a7ea69
syoung1234 <syoung8818@gmail.com>
2022-11-25 19:21:06 +0900

    Add image modal

---
+++ src/examples/ImageModal.vue
@@ -0,0 +1,11 @@
+<template>
+  <transition name="img-modal" appear>
+    <div class="img-modal img-modal-overlay" @click.self="$emit('close')">
+      <div class="img-modal-window mt-5">
+        <div>
+          <slot/>
+        </div>
+      </div>
+    </div>
+  </transition>
+</template>
