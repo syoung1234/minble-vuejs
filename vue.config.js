67c1892c7d34e0a1a5987c75bd92df5eadb7f787
syoung1234 <syoung8818@gmail.com>
2022-11-28 16:51:21 +0900

    Fix port

--- vue.config.js
+++ vue.config.js
@@ -3,11 +3,12 @@ const path = require("path");
 
 module.exports = {
     devServer: {
-        proxy: {
-          '/api': { 
-            target: 'http://127.0.0.1:8080',
-            changeOrigin: true,
-          }, 
+      port: 3000,
+      proxy: {
+        '/api': { 
+          target: 'http://127.0.0.1:8080',
+          changeOrigin: true,
+        }, 
       },
     },
 
