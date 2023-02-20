15f8adb8db1e891ec134c5d0b3da9df14ec4e178
syoung1234 <syoung8818@gmail.com>
2023-02-20 20:40:19 +0900

    feat: add vue-cookies

--- src/main.js
+++ src/main.js
@@ -5,7 +5,8 @@ import router from "./router";
 import "./assets/css/nucleo-icons.css";
 import "./assets/css/nucleo-svg.css";
 import ArgonDashboard from "./argon-dashboard";
-import axios from 'axios'
+import axios from 'axios';
+import VueCookie from "vue-cookies";
 
 
 const appInstance = createApp(App);
@@ -13,4 +14,5 @@ appInstance.config.globalProperties.$axios = axios;
 appInstance.use(store);
 appInstance.use(router);
 appInstance.use(ArgonDashboard);
+appInstance.use(VueCookie);
 appInstance.mount("#app");
